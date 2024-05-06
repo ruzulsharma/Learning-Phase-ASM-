253f35db1474f7d6119cf8c090a1dc20ee54400a
ruzulsharma <ruzulsharma007@gmail.com>
2024-05-06 21:16:32 +0530

    The project is sent

---
+++ healthC/src/main/java/com/example/healthC/HealthCApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCApplication.class, args);
+	}
+
+}
